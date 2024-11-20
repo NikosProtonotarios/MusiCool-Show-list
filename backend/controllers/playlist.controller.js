c70306f9a5d49154ee0e234593d2176c3cd95e4c
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-11-20 22:11:11 +0000

    complete the routes

--- backend/controllers/playlist.controller.js
+++ backend/controllers/playlist.controller.js
@@ -12,28 +12,34 @@ const getAllPlaylists = async (req, res) => {
 
 const createPlaylist = async (req, res) => {
     try {
-        
+        const playlist = new Musical(req.body);
+        await musical.save();
+        return res.status(201).send(playlist);
     } catch (error) {
         console.log(error);
-        return res.status(500).send({msg: "Internal server error", error})
+        return res.status(500).send({ msg: "Internal server error", error });
     }
 };
 
 const updatePlaylist = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const playlist = await Musical.findByIdAndUpdate(id, req.body, { new: true });
+        return res.send(playlist);
     } catch (error) {
         console.log(error);
-        return res.status(500).send({msg: "Internal server error", error})
+        return res.status(500).send({ msg: "Internal server error", error });
     }
 };
 
 const deletePlaylist = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        await Playlist.findByIdAndDelete(id);
+        return res.status(204).send();
     } catch (error) {
         console.log(error);
-        return res.status(500).send({msg: "Internal server error", error})
+        return res.status(500).send({ msg: "Internal server error", error });
     }
 };
 
