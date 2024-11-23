a42873451aba2b2fe4021f3e3a43b3566657aefa
NikosProtonotarios <147170230+NikosProtonotarios@users.noreply.github.com>
2024-11-23 17:21:42 +0000

    add musicalList form

--- client/src/components/musicalList.js
+++ client/src/components/musicalList.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 function MusicalList() {
@@ -8,19 +8,31 @@ function MusicalList() {
         try {
             let response = await axios.get('http://localhost:8080/musicals');
             console.log(response.data);
-            setMusicals(response.data);
+            setMusicals(response.data.data);
         } catch (error) {
             console.error(error);
         }
     }
 
+    useEffect(() => {
+        getAllMusicals();
+    }, []);
+
     return (
         <div>
             <h1 className="musicalTitle">Musicals List</h1>
-            <button onClick={getAllMusicals}>Fetch Musicals</button>
             <div>
                 {musicals.map((musical) => {
-                    return 
+                    return (
+                        <div key={musical._id} className="musicalText">
+                            <div className="musicalTitle">
+                                <h4>{musical.name}</h4>
+                            </div>
+                            <p>{musical.country}</p>
+                            <p>{musical.city}</p>
+                            <p>{musical.date}</p>
+                        </div>        
+                    );
                 })}
             </div>
         </div>
